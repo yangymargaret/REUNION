c2ba3eb598112e7b58d18a86f39ed608810c4017
yangymargaret <yy3@andrew.cmu.edu>
2024-06-09 21:51:18 -0400

    Add files via upload

--- REUNION/__init__.py
+++ REUNION/__init__.py
@@ -1,1 +1,16 @@
+"""
+REUNION
+
+python package for transcription factor binding prediction and regulatory association inference
+"""
+
+from . import utility_1
+from . import test_group_1
+from . import train_pre1
+from . import test_reunion_correlation_1
+from . import test_reunion_compute_pre1
+from . import test_reunion_compute_pre2
+
+__version__ = "0.1.0"
+__author__ = "Maggie"
 
